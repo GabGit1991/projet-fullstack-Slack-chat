680df3b5c640aea13156d6e539de1c1849b495ef
TonyM <tony.mousli@gmail.com>
2023-08-01 11:00:16 +0200

    Base spring projet
    création projet via spring initializr

---
+++ src/main/java/com/projetfullstackslackchat/projetfullstackslackchat/ProjetfullstackslackchatApplication.java
@@ -0,0 +1,13 @@
+package com.projetfullstackslackchat.projetfullstackslackchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetfullstackslackchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetfullstackslackchatApplication.class, args);
+	}
+
+}
