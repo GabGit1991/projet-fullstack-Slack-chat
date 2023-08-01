ddc13641eee161356f509c906294f459cbf53cd7
Tony-Gio <128384303+Tony-Gio@users.noreply.github.com>
2023-08-01 12:31:23 +0200

    Service updated

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import entity.User;
@@ -17,37 +16,73 @@ public class UserService {
 	UserRepository userRepository;
 
 	/**
+	 * METHOD TO CREATE NEW USER
 	 * 
 	 * @param user
-	 * @return on sauve l'utilisateur dans la base de données
+	 * @return we save the User in the database
 	 */
 	public Boolean createUser(User user) {
-		// on vérifier que l'user rentré en paramètre a des attributs cohérents
+		// check that the User in paramater is correctly implemented
 		if (user.getName() != null) {
 			userRepository.save(user);
 			return true;
 		}
-		// si ce n'est pas le cas, on retourne false
+		// if it's not the cas, we return false
 		return false;
 
 	}
 
 	/**
+	 * METHOD TO GET THE LIST OF ALL USERS IN DATABASE
 	 * 
-	 * @return la liste de tous les utilisateurs présents en base de données
+	 * @return the list of all User entities of the detabase
 	 */
 	public List<User> findAllUsers() {
 		return userRepository.findAll();
 	}
 
 	/**
+	 * METHOD TO FIND A USER BY ITS ID
 	 * 
+	 * @param id
+	 * @return an Optional Entity containing a User in function of their id if it
+	 *         exists.
 	 */
-	public ResponseEntity findUserById(Integer id) {
+	public Optional<User> findUserById(Integer id) {
+		return userRepository.findById(id);
+	}
+
+	/**
+	 * METHOD TO DELETE A USER BY ITS ID
+	 * 
+	 * @param id
+	 * @return a Boolean that indicates if a User has been correctly deleted
+	 */
+	public Boolean deleteUserById(Integer id) {
 		Optional<User> optionalUser = userRepository.findById(id);
 		if (optionalUser.isPresent()) {
-
+			userRepository.deleteById(id);
+			return true;
+		} else {
+			return false;
 		}
-
 	}
+
+	/**
+	 * METHOD TO UPDATE A USER BY ITS ID AND A NEW USER
+	 * 
+	 * @param id
+	 * @return a Boolean that indicates if a User has been correctly deleted
+	 */
+	public Boolean updateUserById(Integer id, User user) {
+		Optional<User> optionalUser = userRepository.findById(id);
+		// we check that the User exists and that the id entered match the user's id.
+		if (optionalUser.isPresent() && id == user.getId()) {
+			userRepository.save(user);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 }
